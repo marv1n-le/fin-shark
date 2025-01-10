8c40d47c924de734ed0dc0241031d46215ea1ebf
duc-anh-le <ducanhle.iter@gmail.com>
2025-01-10 20:29:50 +0700

    add identity

--- FinShark/Data/AppDbContext.cs
+++ FinShark/Data/AppDbContext.cs
@@ -1,9 +1,10 @@
 using FinShark.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace FinShark.Data;
 
-public class AppDbContext : DbContext
+public class AppDbContext : IdentityDbContext<AppUser>
 {
     public AppDbContext(DbContextOptions dbContextOptions) : base(dbContextOptions)
     {
--- FinShark/Program.cs
+++ FinShark/Program.cs
@@ -1,7 +1,12 @@
+using System.Text;
 using FinShark.Data;
 using FinShark.Interfaces;
+using FinShark.Models;
 using FinShark.Repository;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 
 namespace FinShark;
 
@@ -16,6 +21,7 @@ public class Program
 
         // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
         builder.Services.AddControllers();
+
         builder.Services.AddEndpointsApiExplorer();
         builder.Services.AddSwaggerGen();
         builder.Services.AddControllers().AddNewtonsoftJson(options =>
@@ -25,6 +31,35 @@ public class Program
         builder.Services.AddDbContext<AppDbContext>(options =>
         {
             options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+        });
+        builder.Services.AddIdentity<AppUser, IdentityRole>(options =>
+        {
+            options.Password.RequireDigit = true;
+            options.Password.RequireLowercase = true;
+            options.Password.RequireUppercase = true;
+            options.Password.RequireNonAlphanumeric = true;
+            options.Password.RequiredLength = 8;
+        }).AddEntityFrameworkStores<AppDbContext>();
+
+        builder.Services.AddAuthentication(options =>
+        {
+            options.DefaultAuthenticateScheme =
+            options.DefaultChallengeScheme = 
+            options.DefaultScheme =
+            options.DefaultForbidScheme =
+            options.DefaultSignInScheme =
+            options.DefaultSignOutScheme = JwtBearerDefaults.AuthenticationScheme;
+        }).AddJwtBearer(options =>
+        {
+            options.TokenValidationParameters = new TokenValidationParameters
+            {
+                ValidateIssuer = true,
+                ValidIssuer = builder.Configuration["Jwt:Issuer"],
+                ValidateAudience = true,
+                ValidAudience = builder.Configuration["Jwt:Audience"],
+                ValidateIssuerSigningKey = true,
+                IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))
+            };
         });
         builder.Services.AddScoped<IStockRepository, StockRepository>();
         builder.Services.AddScoped<ICommentRepository, CommentRepository>();
@@ -39,6 +74,8 @@ public class Program
 
         app.UseHttpsRedirection();
 
+        app.UseAuthentication();
+        
         app.UseAuthorization();
 
         app.MapControllers();
